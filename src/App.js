e4466d9a2e46cab684328a82fb2c56bf28157374
이강현 <khlee92@station3.co.kr>
2019-02-13 19:05:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GlobalStyled } from './globalStyled';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <GlobalStyled />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/globalStyled.js
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+export const GlobalStyled = createGlobalStyle`
+  ${reset}
+
+  body {
+    * {
+      box-sizing: border-box;
+    }
+  }
+`
